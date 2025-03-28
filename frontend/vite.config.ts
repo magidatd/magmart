22102d8076964f3008b054f8de695f29224fad0a
Daniel Magida <daniel.t.magida@gmail.com>
2025-03-28 10:59:35 +0200

    add environment variables, update favicon, and enhance index.html structure

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react(), tailwindcss()],
+	define: {
+		'process.env.VITE_PAYPAL_CLIENT_ID': JSON.stringify(
+			process.env.VITE_PAYPAL_CLIENT_ID,
+		),
+	},
+});

48b8fd2d40bdc317276359ebb98e5b17c316ef53
Daniel Magida <daniel.t.magida@gmail.com>
2025-03-29 19:34:22 +0200

    add gender collection section to home page; create new component and update hero data

--- frontend/src/data/hero-data.ts
+++ frontend/src/data/hero-data.ts
@@ -9,3 +9,22 @@ export const heroData = {
 	desktopPoster: 'src/assets/video/ww_dt_hero_2880x1280_2.jpg',
 	mobilePoster: 'src/assets/video/ww_app_hero_870x1110.jpg',
 };
+
+export const collectionData = {
+	leftCollection: {
+		name: "Women's Collection",
+		image: {
+			url: 'src/assets/hero/2024_homepage_ww_cat_tray_01_870x1110.avif',
+			alt: "Women's Collection",
+		},
+		link: 'Shop Now',
+	},
+	rightCollection: {
+		name: "Men's Collection",
+		image: {
+			url: 'src/assets/hero/2024_homepage_mw_cat_tray_03_870x1110.avif',
+			alt: "Men's Collection",
+		},
+		link: 'Shop Now',
+	},
+};
--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,6 +1,8 @@
 import FlexibleBanner from '@/components/home/flexible-banner';
+import GenderCollectionSection from '@/components/home/gender-collection-section';
 import GlobalBanner from '@/components/home/global-banner';
 import Hero from '@/components/home/hero';
+import { collectionData } from '@/data/hero-data';
 
 const Home = () => {
 	return (
@@ -8,6 +10,7 @@ const Home = () => {
 			<GlobalBanner />
 			<FlexibleBanner />
 			<Hero />
+			<GenderCollectionSection collections={collectionData} />
 		</div>
 	);
 };

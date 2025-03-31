bdff6def729323d080ddf004414550502a768f3d
Daniel Magida <daniel.t.magida@gmail.com>
2025-03-31 21:19:02 +0200

    feat: enhance admin UI with header and sidebar components; add navigation links and logout functionality

--- frontend/src/components/admin/ui/admin-header.tsx
+++ frontend/src/components/admin/ui/admin-header.tsx
@@ -1,4 +1,7 @@
+import SearchBar from '@/components/ui/search-bar';
 import { FaBars } from 'react-icons/fa6';
+import { HiOutlineUser } from 'react-icons/hi2';
+import { Link } from 'react-router-dom';
 
 interface AdminHeaderProps {
 	toggleSideBar: () => void;
@@ -6,23 +9,43 @@ interface AdminHeaderProps {
 
 const AdminHeader: React.FC<AdminHeaderProps> = ({ toggleSideBar }) => {
 	return (
-		<header className='hidden h-16 w-full md:flex items-center relative justify-between px-5 space-x-10 bg-gray-800'>
-			<div className=''>
-				<button onClick={toggleSideBar}>
-					<FaBars size={24} />
-				</button>
-				<h1 className='ml-4 text-xl font-medium'>Admin Dashboard</h1>
-			</div>
-			<div className='flex flex-shrink-0 items-center space-x-4 text-white'>
-				<div className='flex flex-col items-end '>
-					<div className='text-md font-medium '>Unknow Unknow</div>
-
-					<div className='text-sm font-regular'>Student</div>
+		<nav className='hidden md:block bg-gray-300 sticky top-0'>
+			<div className='container mx-auto flex flex-col space-y-2 md:space-y-0 md:flex-row items-center justify-between py-4 px-6'>
+				{/* Left - Logo */}
+				<div className='flex items-center gap-4'>
+					<button onClick={toggleSideBar}>
+						<FaBars size={24} />
+					</button>
+					<Link
+						to='/'
+						className='text-2xl font-medium'
+					>
+						MagMart
+					</Link>
 				</div>
 
-				<div className='h-10 w-10 rounded-full cursor-pointer bg-gray-200 border-2 border-blue-400'></div>
+				{/* Right - Icons */}
+				<div className='flex items-center space-x-4'>
+					<Link
+						to='/'
+						className='block bg-black px-2 rounded text-sm text-white'
+					>
+						Shop
+					</Link>
+					<Link
+						to='/profile'
+						className='hover:text-black'
+					>
+						<HiOutlineUser className='h-6 w-6 text-gray-700' />
+					</Link>
+
+					{/* Search */}
+					<div className='overflow-hidden'>
+						<SearchBar />
+					</div>
+				</div>
 			</div>
-		</header>
+		</nav>
 	);
 };
 
